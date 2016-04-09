1f6e33fa55dd6c812cd6b5208af0873356fa5812
crochet <alicedrake8@gmail.com>
2016-04-08 21:03:44 -0500

    Added files via upload
    Currency converter

---
+++ tomorrow_4.js
@@ -0,0 +1,14 @@
+var currentDate = new Date()
+  var day = currentDate.getDate() 
+  var month = currentDate.getMonth() + 1
+  var year = currentDate.getFullYear()
+  document.write("<b> Today's date is " + month + "/" + day + "/" + year + "</b><br />")
+
+var tomDate = new Date()
+  var day = currentDate.getDate() +1
+  var month = currentDate.getMonth() + 1
+  var year = currentDate.getFullYear()
+  document.write("<b> Tomorrow's date is " + month + "/" + day + "/" + year + "</b>")  
+
+  
+
